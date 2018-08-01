3041fa8a3708009d73b1893dd270251de9c7321e
Chris Parnin <chris.parnin@gmail.com>
2018-07-31 22:52:08 -0400

    Add share mount point.

--- hyperkit/hyperkitrun.sh
+++ hyperkit/hyperkitrun.sh
@@ -21,6 +21,7 @@ fi
 #IMG_CD="-s 3,ahci-cd,/somepath/somefile.iso"
 #IMG_HDD="-s 4,virtio-blk,/somepath/somefile.img"
 PORT="-s 5,virtio-9p,path=/tmp/bakerformac.port.socket,tag=port"
+SHARE="-s 6,virtio-9p,path=/tmp/bakershare.socket,tag=share"
 PCI_DEV="-s 0:0,hostbridge -s 31,lpc"
 LPC_DEV="-l com1,stdio"
 ACPI="-A"
@@ -29,4 +30,4 @@ ACPI="-A"
 FORWARD="-s 7,virtio-sock,guest_cid=3,path=/tmp/,guest_forwards=2000"
 RND="-s 2,virtio-rnd"
 
-$SCRIPTPATH/../vendor/hyperkit $ACPI $MEM $SMP $PCI_DEV $LPC_DEV $NET $PORT $FORWARD $IMG_CD $IMG_HDD $UUID -f kexec,$KERNEL,$INITRD,"$CMDLINE"
+$SCRIPTPATH/../vendor/hyperkit $ACPI $MEM $SMP $PCI_DEV $LPC_DEV $NET $PORT $SHARE $FORWARD $IMG_CD $IMG_HDD $UUID -f kexec,$KERNEL,$INITRD,"$CMDLINE"
