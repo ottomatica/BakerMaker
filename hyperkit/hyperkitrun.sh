06666038e1cfcaa02583838719d15d100d185cdc
Christopher J. Parnin <chris.parnin@gmail.com>
2018-08-02 10:52:27 -0400

    Use virtio-rnd

--- hyperkit/hyperkitrun.sh
+++ hyperkit/hyperkitrun.sh
@@ -30,4 +30,4 @@ ACPI="-A"
 FORWARD="-s 7,virtio-sock,guest_cid=3,path=/tmp/,guest_forwards=2000"
 RND="-s 2,virtio-rnd"
 
-$SCRIPTPATH/../vendor/hyperkit $ACPI $MEM $SMP $PCI_DEV $LPC_DEV $NET $PORT $SHARE $FORWARD $IMG_CD $IMG_HDD $UUID -f kexec,$KERNEL,$INITRD,"$CMDLINE"
+$SCRIPTPATH/../vendor/hyperkit $ACPI $MEM $SMP $PCI_DEV $LPC_DEV $RND $NET $PORT $SHARE $FORWARD $IMG_CD $IMG_HDD $UUID -f kexec,$KERNEL,$INITRD,"$CMDLINE"
